b94c759fed90cf5bb2276794fa103ad5a895e1e9
Ben Liepert <ben.liepert@gmail.com>
2023-08-26 10:19:29 -0400

    Removing run-wasm pieces

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        //console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
+        console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
 
         wasm_bindgen_futures::spawn_local(run());
     }
